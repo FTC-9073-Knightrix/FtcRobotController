fa8f6aac57badcc48a2bbc1cc5d9b133cafaf857
meshhi13 <hhhimesh.131@gmail.com>
2021-12-13 15:31:12 -0500

    basic drive for autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoProgram.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
+@Autonomous(name = "testAuto", group = "Knightrix")
+public class autoProgram extends LinearOpMode {
+
+    DcMotor leftDrive;
+    DcMotor rightDrive;
+    public void moveForward(double time) {
+        leftDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftDrive.setPower(1);
+        rightDrive.setPower(1);
+        sleep((long)time);
+    }
+    public void turnRight(double time){
+        leftDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftDrive.setPower(1);
+        rightDrive.setPower(1);
+        sleep((long)time);
+    }
+    public void turnLeft(double time){
+        leftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftDrive.setPower(1);
+        rightDrive.setPower(1);
+        sleep((long)time);
+    }
+    public void setOff(){
+        //setting power to zero (stop)
+        leftDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+    }
+
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+        leftDrive = hardwareMap.get(DcMotor.class, "left_drive");
+        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+
+        waitForStart();
+        moveForward(1000);
+        turnLeft(1000);
+        moveForward(1000);
+
+
+
+        }
+    }
+
