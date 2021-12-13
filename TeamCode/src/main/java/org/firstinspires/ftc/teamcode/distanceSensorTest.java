f9f6988e11185ccc0f268e59b4f6c5b80601aa16
nicole <nicoleortuno1550@gmail.com>
2021-12-13 16:25:04 -0500

    distance sensor moves motor with specific distance

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/distanceSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/distanceSensorTest.java
@@ -1,33 +1,55 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 
-@TeleOp (name = "distanceSensorOpMode", group = "sensors9073")
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+
+@TeleOp(name = "Sensor: REV2mDistance", group = "Sensor")
 
 public class distanceSensorTest extends LinearOpMode {
-    DistanceSensor distance;
-    DcMotor motor;
+
+    private DistanceSensor sensorRange;
+    private DcMotor motor = null;
 
     @Override
     public void runOpMode() {
-        // Get the distance sensor and motor from hardwareMap
-        distance = hardwareMap.get(DistanceSensor.class, "DistanceTest");
-        motor = hardwareMap.get(DcMotor.class, "Motor");
+        // you can use this as a regular DistanceSensor.
+        sensorRange = hardwareMap.get(DistanceSensor.class, "DistanceTest");
+        motor = hardwareMap.get(DcMotor.class, "left_drive");
 
-        // Loop while the Op Mode is running
+        // you can also cast this to a Rev2mDistanceSensor if you want to use added
+        // methods associated with the Rev2mDistanceSensor class.
+        Rev2mDistanceSensor sensorTimeOfFlight = (Rev2mDistanceSensor)sensorRange;
+        motor.setDirection(DcMotor.Direction.FORWARD);
+
+        telemetry.addData(">>", "Press start to continue");
+        telemetry.update();
+
         waitForStart();
-        while (opModeIsActive()){
-            double distance1 = distance.getDistance(DistanceUnit.CM);
-            //Add data and format correctly
-            telemetry.addData("status", "running");
-            telemetry.addData("distance: ", distance.getDistance(DistanceUnit.CM));
-            //Consistently update the data while the Op Mode is running
-            telemetry.update();
-            }
+        while(opModeIsActive()) {
+            // generic DistanceSensor methods.
+            if (sensorRange.getDistance(DistanceUnit.INCH)>=6){
+                motor.setPower(1);
+            } else{motor.setPower(0);}
+            telemetry.addData("deviceName",sensorRange.getDeviceName() );
+            telemetry.addData("range", String.format("%.01f mm", sensorRange.getDistance(DistanceUnit.MM)));
+            telemetry.addData("range", String.format("%.01f cm", sensorRange.getDistance(DistanceUnit.CM)));
+            telemetry.addData("range", String.format("%.01f m", sensorRange.getDistance(DistanceUnit.METER)));
+            telemetry.addData("range", String.format("%.01f in", sensorRange.getDistance(DistanceUnit.INCH)));
 
+            // Rev2mDistanceSensor specific methods.
+            telemetry.addData("ID", String.format("%x", sensorTimeOfFlight.getModelID()));
+            telemetry.addData("did time out", Boolean.toString(sensorTimeOfFlight.didTimeoutOccur()));
+
+            telemetry.update();
         }
     }
+
+}
