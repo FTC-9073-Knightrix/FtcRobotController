0e75be9adc40a633bb1a980aa7ae4afac7ba2fb7
nicole <nicoleortuno1550@gmail.com>
2022-01-22 03:40:20 -0500

    movement direction fixed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive.java
@@ -162,8 +162,8 @@ public class drive extends LinearOpMode {
             // *************************
             // **** DRIVE THE ROBOT ****
             // *************************
-            double drive = gamepad1.left_stick_y;
-            double turn = gamepad1.right_stick_x;
+            double drive = -gamepad1.left_stick_y;
+            double turn = -gamepad1.right_stick_x;
 
             // Combine drive and turn for blended motion.
             LeftPower  = drive - turn;
@@ -178,8 +178,8 @@ public class drive extends LinearOpMode {
             }
 
             // Output the safe vales to the motor drives.
-            leftDrive.setPower(LeftPower);
-            rightDrive.setPower(RightPower);
+            leftDrive.setPower(-LeftPower);
+            rightDrive.setPower(-RightPower);
 
 
 
